195725203bf13490c0684f738e3e50e2504735ca
Nelson <nyang3006@baysidehighschool.org>
2017-07-27 12:28:14 -0400

    added js file

---
+++ jsj.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$(this.body).scrollTop(0);	
+
+	// /********Drop Down********/
+	var n_topic = $('#b_t_drp');
+	var n_tpcs = $('#dropTpc');
+	n_topic.hover(drop,nvmdrop);
+
+	n_tpcs.hover(drop,nvmdrop);
+
+	function drop(){
+		n_tpcs.css("visibility","visible");
+	}
+	function nvmdrop(){
+		n_tpcs.css("visibility","hidden");
+					
+	}
+});
