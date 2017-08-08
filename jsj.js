c655e2dd6fb4b396ac7ed8296b7c1488b606461e
Nelson <nyang3006@baysidehighschool.org>
2017-08-08 10:35:27 -0400

    fixed

--- jsj.js
+++ jsj.js
@@ -22,13 +22,18 @@
 window.onbeforeunload = function () {
 	window.scrollTo(0, 0);
 }
+
 $(document).ready(function(){
 	$(this.body).scrollTop(0);	
 
 	// /********Drop Down********/
 	var n_topic = $('#b_t_drp');
 	var n_tpcs = $('#dropTpc');
+	var title=document.getElementsByTagName("title")[0].innerHTML;
 	var nav = $('#b_nav');
+	if(title=='Pet Abuse' || title=='War'){
+		nav = $('#ab_nav');
+	}
 	var n = $('#b_t_drp').position();
 	var w = $('#b_t_drp').width();
 	console.log(w)
