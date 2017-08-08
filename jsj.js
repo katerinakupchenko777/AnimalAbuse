41b4c352359f028ecdcbe967b9cc7a2647a01d90
Nelson <nyang3006@baysidehighschool.org>
2017-08-08 09:53:52 -0400

    fixed nav bar removed everything

--- jsj.js
+++ jsj.js
@@ -1,14 +1,38 @@
+// $(document).ready(function(){
+// 	$(this.body).scrollTop(0);	
+
+// 	// /********Drop Down********/
+// 	var n_topic = $('#b_t_drp');
+// 	var n_tpcs = $('#dropTpc');
+	
+// 	n_topic.hover(drop,nvmdrop);
+
+// 	n_tpcs.hover(drop,nvmdrop);
+// 	var drop = false;
+// 	function drop(){
+// 		n_tpcs.css("visibility","visible");
+
+// 	}
+// 	function nvmdrop(){
+// 		n_tpcs.css("visibility","hidden");
+					
+// 	}
+	
+// });
+
 $(document).ready(function(){
 	$(this.body).scrollTop(0);	
 
 	// /********Drop Down********/
 	var n_topic = $('#b_t_drp');
 	var n_tpcs = $('#dropTpc');
-	
-	n_topic.hover(drop,nvmdrop);
+	var nav = $('#b_nav');
+	var n = $('#b_t_drp').position();
+	var w = $('#b_t_drp').width();
+	console.log(w)
+	// n_tpcs.hover(drop,nvmdrop);
 
-	n_tpcs.hover(drop,nvmdrop);
-	var drop = false;
+	var droped = false;
 	function drop(){
 		n_tpcs.css("visibility","visible");
 
@@ -17,5 +41,24 @@ $(document).ready(function(){
 		n_tpcs.css("visibility","hidden");
 					
 	}
+
+	nav.hover(function(e){
+
+		console.log(e.pageX)
+		if( e.pageX >= n.left-60 && e.pageX <= n.left-60+w+90){
+			drop()	
+		}
+
+	},nvmdrop);
+
+	n_topic.hover(function(e){
+
+		console.log(e.pageX)
+		if( e.pageX >= n.left-60 && e.pageX <= n.left-60+w+90){
+			drop()	
+		}
+
+	});
+
 	
 });
